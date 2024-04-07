0588078dff043179e1d1f6863a711d1e4d4af47c
Kuber144 <kuberjain144@gmail.com>
2024-04-07 12:55:02 +0530

    Added models and backend database for code checker

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-import express from "express";
+import express, { json } from "express";
+import connectToMongo from "./db.js";
 import cors from "cors";
 import judgeRouterLang from "./routes/judge/getLangs.js";
 import judgeRouterCode from "./routes/judge/executeCode.js";
 import bodyParser from "body-parser";
-
+connectToMongo();
 const app = express();
 
 const PORT = process.env.PORT || 8000; //running on port 8000
 app.use(cors()); //Apply cors
-app.use(bodyParser.json()); // Parse JSON-encoded bodies
+app.use(json({ limit: "25mb" })); // Parse JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.get("/", (req, res) => {
   res.send("Application is running!");
